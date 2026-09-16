b7fa9529fd8164e25dd8bb9e4b79cf944251640f
aliarda2017 <43536534+aliarda2017@users.noreply.github.com>
2026-09-16 22:59:33 +0300

    Add files via upload

--- yayin-ayar.js
+++ yayin-ayar.js
@@ -1,5 +1,5 @@
 window.VUSLAT_RADIO_CONFIG = {
   statusUrl: "https://vuslat-yayin-durumu.vuslatvakti2505.workers.dev/",
-  jingleUrl: "vuslat-cingil.mp4",
+  jingleUrl: "vuslat-cingil.mp3",
   pollMs: 30000
 };
